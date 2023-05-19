56bb837ccfe0fd7cc02102f99a6a874a272610eb
emin4454 <132940987+emin4454@users.noreply.github.com>
2023-05-19 22:38:07 +0300

    sonupdateler

---
+++ iletisim.php
@@ -0,0 +1,8 @@
+<?php
+ echo "İsminiz=".$_POST['isim'];
+ echo "<br>Soyisminiz:".$_POST['soyisim'];
+ echo "<br>Telefon numaranız:".$_POST['telefonNo'];
+ echo "<br>Cinsiyetiniz:".$_POST['cinsiyet'];
+ echo "<br>Email:".$_POST['emailIletisim'];
+ echo "<br>Mesajınız:".$_POST['textAlani'];
+ ?>
